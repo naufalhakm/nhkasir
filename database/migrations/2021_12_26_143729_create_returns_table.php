059c791d23e6eba1669e32100aaddedf76e4bd9e
Naufal_Hakim <naufalhakm@gmail.com>
2022-01-04 15:11:37 +0700

    initial commit

---
+++ database/migrations/2021_12_26_143729_create_returns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReturnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('returns', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->string('user');
+            $table->string('nama_kegiatan');
+            $table->integer('return');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('returns');
+    }
+}
---
+++ database/migrations/2021_12_26_150430_create_rewards_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRewardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rewards', function (Blueprint $table) {
+            $table->id();
+            $table->integer('jenis');
+            $table->integer('role');
+            $table->string('reward');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rewards');
+    }
+}
